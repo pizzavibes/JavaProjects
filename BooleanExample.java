ef13af9df734a72dc3cf7c968f61dbed281994dc
pizzavibes <143003325+pizzavibes@users.noreply.github.com>
2023-08-23 09:53:54 -0500

    Add files via upload

---
+++ BooleanExample.java
@@ -0,0 +1,15 @@
+package booleanexample ;
+
+/**
+ *
+ * @author David Santiago Álvarez Ríos
+ */
+public class BooleanExample {
+
+    
+    public static void main(String[] args) {
+        boolean intsCanHoldDecimals = false ;
+        System.out.println(intsCanHoldDecimals);
+    }
+    
+}
