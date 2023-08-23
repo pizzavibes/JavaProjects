603c4485944181acfabcdc77922aeccab8ce1cd1
pizzavibes <143003325+pizzavibes@users.noreply.github.com>
2023-08-23 09:50:39 -0500

    Add files via upload

---
+++ AdditionSubstractionExample.java
@@ -0,0 +1,21 @@
+
+package additionsubstractionexample;
+
+/**
+ *
+ * @author David Santiago Álvarez Ríos
+ */
+public class AdditionSubstractionExample {
+
+    
+    public static void main(String[] args) {
+        int zebrasInZoo = 8;
+        int giraffesInZoo = 4;
+        int animalsInZoo = zebrasInZoo + giraffesInZoo;
+        System.out.println(animalsInZoo);
+        
+        int numZebrasAfterTrade = zebrasInZoo-2 ;
+        System.out.println(numZebrasAfterTrade);
+    }
+    
+}
