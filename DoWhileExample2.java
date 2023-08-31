023949b1877bb2f8d98b6611fe39ee05918510ed
pizzavibes <143003325+pizzavibes@users.noreply.github.com>
2023-08-31 09:45:12 -0500

    Add files via upload

---
+++ DoWhileExample2.java
@@ -0,0 +1,12 @@
+package dowhileexample2;
+
+public class DoWhileExample2{
+public static void main(String[]args){
+
+do{
+    System.out.println("infinitive do while loop");
+}while (true);
+        
+  
+}
+}
