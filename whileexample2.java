d177fa168ef70914e4b5abf6c813c74f81355f5a
pizzavibes <143003325+pizzavibes@users.noreply.github.com>
2023-08-31 09:21:37 -0500

    Add files via upload

---
+++ whileexample2.java
@@ -0,0 +1,12 @@
+package whileexample2 ;
+
+public class whileexample2 {
+    public static void main(String[]args){
+        
+        while (true) {
+            System.out.println("Infinitive while loop ");
+            
+        }
+}
+
+}
