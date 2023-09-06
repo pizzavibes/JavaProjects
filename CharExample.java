291d2da97c492a23fd63979f6714d024e7f8b27a
pizzavibes <143003325+pizzavibes@users.noreply.github.com>
2023-09-06 08:42:28 -0500

    Add files via upload

---
+++ CharExample.java
@@ -0,0 +1,11 @@
+
+package charexample;
+
+
+public class CharExample {
+	public static void main(String[] args) {   
+     char expectedGrade = 'C'; 
+     System.out.println(expectedGrade);
+	}
+}
+
