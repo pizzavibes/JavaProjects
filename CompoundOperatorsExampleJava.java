770deaa37e64b33065181f064b3d2fe729bbd109
pizzavibes <143003325+pizzavibes@users.noreply.github.com>
2023-09-06 08:44:13 -0500

    Add files via upload

---
+++ CompoundOperatorsExampleJava.java
@@ -0,0 +1,12 @@
+
+package compoundoperatorsexample.java;
+public class CompoundOperatorsExampleJava {
+	public static void main(String[] args) {   
+		int numCookies = 17;
+    numCookies -= 3;
+    System.out.println(numCookies);
+    numCookies /= 2;
+    // Add your code above
+    System.out.println(numCookies);
+	}
+}
