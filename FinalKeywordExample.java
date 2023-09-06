d0bd2533cee58bdeccfdf05d3d39c473b7aeebd4
pizzavibes <143003325+pizzavibes@users.noreply.github.com>
2023-09-06 08:54:56 -0500

    Add files via upload

---
+++ FinalKeywordExample.java
@@ -0,0 +1,11 @@
+
+package finalkeywordexample;
+
+public class FinalKeywordExample {
+	public static void main(String[] args) { 
+    double pi = 3.14;
+    System.out.println(pi);  
+    pi = 2*pi;  
+		
+	}
+}
