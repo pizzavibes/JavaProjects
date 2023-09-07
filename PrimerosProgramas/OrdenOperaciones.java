d36649334f7327bedf2954fc0b1e2c3f1f4abfc7
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-07 18:41:12 -0500

    Add files via upload

---
+++ PrimerosProgramas/OrdenOperaciones.java
@@ -0,0 +1,17 @@
+
+package ordenoperaciones;
+
+public class OrdenOperaciones {
+
+
+           
+    public static void main(String[] args) {
+   int operacion1= 2*3+(5/3);
+   int operacion2= (3*5+(4*8+3));
+   int operacion3= 8/4+(12*4+5);
+        System.out.println(operacion1);
+        System.out.println(operacion2);
+        System.out.println(operacion3);
+     }
+    
+}
