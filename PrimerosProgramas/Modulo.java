0fe6b891467a0ab0f3b12d328358707d897f95e2
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-07 18:55:34 -0500

    Add files via upload

---
+++ PrimerosProgramas/Modulo.java
@@ -0,0 +1,11 @@
+package modulo;
+
+public class Modulo {
+
+    public static void main(String[] args) {
+       int  asistentesConcierto= 500;
+       int meetAndGreet= asistentesConcierto%30;
+        System.out.println(meetAndGreet);
+    }
+    
+}
