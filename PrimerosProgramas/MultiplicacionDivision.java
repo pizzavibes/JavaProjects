3c787bf656539f7622a80d487e4dec5a8e99ee23
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-07 18:50:05 -0500

    Add files via upload

---
+++ PrimerosProgramas/MultiplicacionDivision.java
@@ -0,0 +1,22 @@
+
+package multiplicaciondivision;
+
+public class MultiplicacionDivision {
+
+    
+public static void main(String[] args) {
+ double subtotal = 4000.98;
+ double iva = 205 ;
+ double total= subtotal+(subtotal*iva);
+ double porPersona = total/3;
+ 
+    System.out.println(total);
+    System.out.println(porPersona);
+ 
+         
+         
+         
+         
+}
+    
+}
