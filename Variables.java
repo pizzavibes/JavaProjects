f76f3ba24af3f3acf3ae06d6a5b9758d0aa878ef
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-07 17:22:05 -0500

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class Variables {
+    public static void main(String[] args) {
+     String name = "Jonathan Joestar";
+     int birthYear = 1868;
+        System.out.println(name);
+        System.out.println(birthYear);
+     
+        }
+    
+}
