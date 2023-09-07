13c58200d172e3225095a8b1c6839963a414dcfb
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-07 17:52:42 -0500

    Add files via upload

---
+++ PrimerosProgramas/OperadoresRelacionales2.java
@@ -0,0 +1,17 @@
+package operadoresrelacionales2;
+
+public class OperadoresRelacionales2 {
+
+    public static void main(String[] args) {
+       int mym=50;
+       int sparkies=40;
+       int  ventaMym= 35;
+       int ventaSparkies=35;
+       boolean diferenteNumeroDeDulces= mym == sparkies;
+       boolean mismasVentas= ventaMym==ventaSparkies;
+        System.out.println(diferenteNumeroDeDulces);
+        System.out.println(mismasVentas);
+       
+    }
+    
+}
