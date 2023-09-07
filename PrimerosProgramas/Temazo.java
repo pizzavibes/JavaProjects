93986d6c5d5f2254468b3bf45df235171555168e
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-07 17:29:47 -0500

    Add files via upload

---
+++ PrimerosProgramas/Temazo.java
@@ -0,0 +1,12 @@
+
+package temazo;
+
+public class Temazo {
+
+    
+    public static void main(String[] args) {
+       String letraInicial = "En un cafe se vieron por casualidad, cansados en el alma de tanto andar";
+        System.out.println(letraInicial);
+    }
+    
+}
