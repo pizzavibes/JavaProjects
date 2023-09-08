c54f755cbfb1931e62909f961107edb28caa4afe
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-07 19:35:04 -0500

    Add files via upload

---
+++ PrimerosProgramas/Dinero.java
@@ -0,0 +1,15 @@
+
+package dinero;
+
+public class Dinero {
+
+    public static void main(String[] args) {
+        int disponible= 200000;
+        disponible -=10000;
+        System.out.println(disponible);
+        disponible /= 3;
+        System.out.println(disponible);
+           
+    }
+    
+}
