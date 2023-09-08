617e973781b44963b66c21073f6ccc1bfc16a8e6
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-07 19:47:02 -0500

    Add files via upload

---
+++ PrimerosProgramas/SumaResta.java
@@ -0,0 +1,15 @@
+package sumaresta;
+public class SumaResta {
+    public static void main(String[] args) {
+        int perros=6;
+        int gatos=4;
+        int conejos=6;
+        int hamsters=10;
+        int animalesEnTienda= perros+gatos+conejos+hamsters;
+        System.out.println(animalesEnTienda);
+        
+        int perrosTrasCompra = perros-3;
+        System.out.println(perrosTrasCompra);
+    }
+    
+}
