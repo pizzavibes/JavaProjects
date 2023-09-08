fa77ea6e13f3276ac35ca7c581f69bbf63c4eaeb
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-07 19:21:43 -0500

    Add files via upload

---
+++ PrimerosProgramas/Igual.java
@@ -0,0 +1,12 @@
+package igual;
+
+public class Igual {
+
+    public static void main(String[] args) {
+      String parte1= "Personal de limpieza solicitado en pasillo 5";
+      String parte2="Personal de limpieza solicitado en pasillo 5";
+      String parte3="Con clorox limpia tu garganta :D";
+        System.out.println(parte1.equals(parte2));
+        System.out.println(parte2.equals(parte3));
+    }
+}
