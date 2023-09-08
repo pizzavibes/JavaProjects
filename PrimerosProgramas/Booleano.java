6d4b84a10a8bbe4aaafa6cd84dc3de4802f2e7c1
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-07 19:42:56 -0500

    Add files via upload

---
+++ PrimerosProgramas/Booleano.java
@@ -0,0 +1,8 @@
+package booleano;
+public class Booleano {
+    public static void main(String[] args) {
+        boolean siEstoEsFalsoYoExisto= false;
+        System.out.println(siEstoEsFalsoYoExisto);
+    }
+    
+}
