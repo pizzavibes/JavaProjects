7418ac2b76cbefa3e2e29a0d5d8dd3ddb7d19781
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-07 19:29:39 -0500

    Add files via upload

---
+++ PrimerosProgramas/ConcatenacionEx.java
@@ -0,0 +1,10 @@
+package concatenacionex;
+public class ConcatenacionEx {
+    public static void main(String[] args) {
+        int diasTotales = 31;
+        String diass= "dias totales";
+        String descripcionDias= "El mes tiene "+diasTotales+" "+diass;
+        System.out.println(descripcionDias);
+    }
+    
+}
