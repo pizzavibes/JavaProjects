8681c7f0edc1b754f215173f70ccd4d44498f6dc
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-07 19:56:58 -0500

    Add files via upload

---
+++ PrimerosProgramas/Estrella.java
@@ -0,0 +1,14 @@
+package estrella;
+public class Estrella {
+    public static void main(String[] args) {
+        System.out.println("Ey quieres ver una estrella? Mira: ");
+        System.out.println("                   *                ");
+        System.out.println("            ***************                    ");
+        System.out.println("              ***********  ");
+        System.out.println("               ********* ");
+        System.out.println("               * *   * * ");
+        System.out.println("               *       *  ");
+       
+    }
+    
+}
