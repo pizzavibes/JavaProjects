267ea9dac4330ea4135b29317b70f16ccadbf261
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-08 22:18:01 -0500

    Add files via upload

---
+++ Lazos/BreakDoWhile.java
@@ -0,0 +1,17 @@
+package breakdowhile;
+public class BreakDoWhile {
+    public static void main(String[] args) {
+   int fritz = 3;
+        do {
+            if (fritz==4) {
+                fritz++;
+                break;
+            
+        } 
+            System.out.println(fritz);
+                    fritz++;
+        }while(fritz<=13);
+        
+    }
+    
+}
