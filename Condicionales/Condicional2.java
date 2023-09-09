963d32119447641949eb72935a95e97e6aa96530
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-08 22:36:19 -0500

    Add files via upload

---
+++ Condicionales/Condicional2.java
@@ -0,0 +1,15 @@
+package condicional2;
+public class Condicional2 {
+    public static void main(String[] args) {
+        int pa = 23;
+        int pu = 43;
+        if (pa+pu<45) {
+            System.out.println("pa+pu es menor a 45");
+            
+        }
+        else{
+            System.out.println("pa+pu es mayor a 45");
+        }
+    }
+    
+}
