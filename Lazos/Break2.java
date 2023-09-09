f95a64e601afd22c66f8bfec3ef5c192350c0e31
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-08 21:33:19 -0500

    Add files via upload

---
+++ Lazos/Break2.java
@@ -0,0 +1,17 @@
+
+package break2;
+public class Break2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=3; i++) {
+            
+        
+        for (int h = 1; h <= 3; h++) {
+            if (i==2&&h==2) {
+                break;
+                           }
+            System.out.println(i+" "+ h );
+        }
+        
+    }
+    } 
+}
