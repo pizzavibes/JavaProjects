026c2d6b1fc80c2f97ea554a3804fa7fdce769db
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-08 22:36:42 -0500

    Add files via upload

---
+++ Condicionales/Condicional3.java
@@ -0,0 +1,32 @@
+package condicional3;
+public class Condicional3 {
+    public static void main(String[] args) {
+        int val=32;
+        if (val<30) {
+            System.out.println("perdiste que bobo jajajaja");
+        }
+        else if(val>=20 && val<30){
+                System.out.println("No pasaste");
+                }
+        else if(val>=30 && val<40)
+            System.out.println("Pasaste, yei");
+        else if(val>=40 && val<50){
+             System.out.println("Pasaste sobre 40, crack fiera, mastodonte, genio, hermano hermanazo");
+        }else{
+                     System.out.println("Papi no invente no puede sacarse tanto");
+                     
+                
+     }
+                
+        
+        
+  }
+        
+
+}
+              
+        
+        
+    
+    
+
