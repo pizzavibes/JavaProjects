056471917701ab05f4134d43ace354d597c6c035
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-08 21:23:29 -0500

    Add files via upload

---
+++ Lazos/Break.java
@@ -0,0 +1,15 @@
+package pkgbreak;
+
+public class Break {
+
+    public static void main(String[] args) {
+        for (int waos = 0; waos < 10; waos++) {
+            if (waos==5) {
+                break;
+            }
+            System.out.println(waos);
+        }
+        
+    }
+    
+}
