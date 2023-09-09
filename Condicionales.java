1d85053596b95c82c7d94f50788fc9693963c125
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-09-08 20:31:44 -0500

    Add files via upload

---
+++ Condicionales.java
@@ -0,0 +1,13 @@
+package condicionales;
+
+public class Condicionales {
+
+      public static void main(String[] args) {
+       int perros = 14;
+       int gatos = 23;
+          if (perros+gatos>30) {
+              System.out.println("Hay mas de treinta animales"); 
+          }
+    }
+    
+}
