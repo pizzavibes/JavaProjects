12c9a0ee84b3261b03c8a46b06e170a6beb6dca3
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-10-05 08:54:28 -0500

    Add files via upload

---
+++ Segundo_corte/JOptionPane/Welcome.java
@@ -0,0 +1,11 @@
+package welcome;
+import javax.swing.JOptionPane;
+public class Welcome {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null,"El que lo lea es gay");
+        System.exit(0);
+    }
+    
+}
